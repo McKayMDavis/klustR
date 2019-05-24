818d646db132fe35528b98029ee0830bf5dbef65
McKay Davis <mckaymdavis@gmail.com>
2019-05-24 12:23:18 -0600

    minor edits to code and transitions. Add tooltip to barcharts.

--- inst/htmlwidgets/pcplot.js
+++ inst/htmlwidgets/pcplot.js
@@ -6,12 +6,27 @@ HTMLWidgets.widget({
 
   initialize: function(el, width, height) {
 
-    //Initialize an svg
+    //A little style goes a long way
     d3.select(el)
+      .style("font-family", "Arial, Helvetica, sans-serif");
+
+    //Initialize an svg
+    var svg = d3.select(el)
       .append("svg")
       .attr("width", width)
-      .attr("height", height)
-      .style("font-family", "Arial, Helvetica, sans-serif");
+      .attr("height", height);
+
+    //Tooltip
+    var tooltip = d3.select(el)
+      .append("div")
+      .attr("class", "tooltip")
+      .attr("id", "tooltip")
+      .style("position", "absolute")
+      .style("width", "200px")
+      .style("height", "28px")
+      .style("pointer-events", "none")
+      .style("font-weight", "bold")
+      .style("font-size", "14px");
 
     //Return a bunch of values accessible in the instance object
     return {
@@ -62,17 +77,6 @@ HTMLWidgets.widget({
             d3.scaleOrdinal().range(colorScheme) :
             d3.scaleOrdinal(d3[colorScheme]);
 
-        //Tooltip
-        var tooltip = d3.select(el)
-          .append("div")
-          .attr("class", "tooltip")
-          .style("position", "absolute")
-          .style("width", "200px")
-          .style("height", "28px")
-          .style("pointer-events", "none")
-          .style("font-weight", "bold")
-          .style("font-size", "14px");
-
         //Append plot
         var svg = d3.select("svg")
           .append("g")
@@ -103,14 +107,12 @@ HTMLWidgets.widget({
               .style("font-weight", "normal");
           })
           .on("click", function() {
-            d3.select("svg").selectAll("*").transition()
-              .duration(500)
-              .style("opacity", 0)
+            d3.select("svg").selectAll("*")
               .remove();
               instance.pc = "PC1";
               instance.barChart(instance);
         	})
-          .text("PC1 - " + Number.parseFloat(pve1).toPrecision(4) + "%");
+          .text("PC1 - " + Number.parseFloat(pve1).toFixed(2) + "%");
 
         //Y-axis
         svg.append("g")
@@ -134,14 +136,12 @@ HTMLWidgets.widget({
               .style("font-weight", "normal");
           })
           .on("click", function() {
-            d3.select("svg").selectAll("*").transition()
-              .duration(500)
-              .style("opacity", 0)
+            d3.select("svg").selectAll("*")
               .remove();
               instance.pc = "PC2";
               instance.barChart(instance);
         	})
-          .text("PC2 - " + Number.parseFloat(pve2).toPrecision(4) + "%");
+          .text("PC2 - " + Number.parseFloat(pve2).toFixed(2) + "%");
 
         //Draw points and add dynamic affects for mouseover points
         svg.selectAll(".dot")
@@ -161,10 +161,8 @@ HTMLWidgets.widget({
               .transition()
               .duration(200)
               .style("opacity", 0.1);
-            tooltip.transition()
-              .duration(200)
-              .style("opacity", 1);
-            tooltip.html(d._row)
+            tooltip.style("opacity", 1)
+              .html(d._row)
               .style("left", (d3.event.pageX + 5) + "px")
               .style("top", (d3.event.pageY - 28) + "px");
           })
@@ -173,10 +171,7 @@ HTMLWidgets.widget({
               .delay(100)
               .duration(500)
               .style("opacity", 1);
-            tooltip.transition()
-              .delay(100)
-              .duration(200)
-              .style("opacity", 0);
+            tooltip.style("opacity", 0);
           });
 
         //Draw legend area
@@ -187,19 +182,22 @@ HTMLWidgets.widget({
           .attr("class", "legend")
           .attr("transform", (d, i) => { return "translate(0," + i * 20 + ")"; })
           .on("click", function(d) {
-            var currentGroupOpacity = svg.selectAll('#dot' + d).style("opacity"),
-                allOthersOpacity = svg.selectAll('.dot:not(#dot' + d + ')').style("opacity");
-            if (allOthersOpacity < 1 && currentGroupOpacity < 1) {
-              svg.selectAll('#dot' + d).style("opacity", 1);
-              svg.selectAll('.dot:not(#dot' + d + ')').transition()
+            var curr = '#dot' + d,
+              oth = '.dot:not(' + curr + ')',
+              currOpacity = svg.selectAll(curr).style("opacity"),
+              othersOpacity = svg.selectAll(oth).style("opacity");
+
+            if (othersOpacity < 1 && currOpacity < 1) {
+              svg.selectAll(curr).style("opacity", 1);
+              svg.selectAll(oth).transition()
                 .style("opacity", 0.1);
 
-            } else if (allOthersOpacity < 1) {
-              svg.selectAll('.dot:not(#dot' + d + ')').transition()
+            } else if (othersOpacity < 1) {
+              svg.selectAll(oth).transition()
                 .style("opacity", 1);
             } else {
-              svg.selectAll('#dot' + d).style("opacity", 1);
-              svg.selectAll('.dot:not(#dot' + d + ')').transition()
+              svg.selectAll(curr).style("opacity", 1);
+              svg.selectAll(oth).transition()
                 .style("opacity", 0.1);
             }
           });
@@ -223,99 +221,106 @@ HTMLWidgets.widget({
 ////////////////////////////////////////////////////////////////////////////////
 // Function to draw a bar chart of PC contributions
 ////////////////////////////////////////////////////////////////////////////////
-    barChart: function (instance) {
+      barChart: function (instance) {
 
-      //Set current plot type
-      instance.currPlot = "bar";
+        //Set current plot type
+        instance.currPlot = "bar";
 
-      var x = instance.x,
-        component = instance.pc,
-        margin = instance.margin,
-        width = instance.width,
-        height = instance.height;
+        var x = instance.x,
+          component = instance.pc,
+          margin = instance.margin,
+          width = instance.width,
+          height = instance.height;
 
-      //User opts
-      barColor = x.barColor;
+        //User opts
+        barColor = x.barColor;
 
-      //Calculations
-      cont = x.cont;
-      thresh = x.thresh;
+        //Calculations
+        cont = x.cont;
+        thresh = x.thresh;
 
-      //Settup x
-      var xValue = d => d._row,
-        xScale = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-        xMap = d => xScale(xValue(d)),
-        xAxis = d3.axisBottom(xScale);
+        //Settup x
+        var xValue = d => d._row,
+          xScale = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+          xMap = d => xScale(xValue(d)),
+          xAxis = d3.axisBottom(xScale);
 
-      //Settup y
-      var yValue = d => d[component],
-        yScale = d3.scaleLinear().rangeRound([height, 0]),
-        yMap = d => yScale(yValue(d)),
-        yAxis = d3.axisLeft(yScale);
+        //Settup y
+        var yValue = d => d[component],
+          yScale = d3.scaleLinear().rangeRound([height, 0]),
+          yMap = d => yScale(yValue(d)),
+          yAxis = d3.axisLeft(yScale);
 
-      //Append plot
-      var svg = d3.select("svg")
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        //Append plot
+        var svg = d3.select("svg")
+          .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      //Domains
-    	xScale.domain(cont.map(xValue));
-    	yScale.domain([0, d3.max(cont, yValue)]);
+        //Domains
+      	xScale.domain(cont.map(xValue));
+      	yScale.domain([0, d3.max(cont, yValue)]);
 
-      //X-axis
-    	svg.append("g")
-    	  .attr("transform", "translate(0," + height + ")")
-    	  .call(xAxis);
+        //X-axis
+      	svg.append("g")
+      	  .attr("transform", "translate(0," + height + ")")
+      	  .call(xAxis);
 
-      //Y-axis
-    	svg.append("g")
-    	  .call(yAxis);
+        //Y-axis
+      	svg.append("g")
+      	  .call(yAxis);
 
-      //Add bars
-    	svg.selectAll(".bar")
-    	  .data(cont)
-    	  .enter().append("rect")
-    	  .style("fill", barColor)
-    	  .attr("class", "bar")
-    	  .attr("x", d => xMap(d))
-        .attr("y", d => yMap(d))
-    	  .attr("width", xScale.bandwidth())
-    	  .attr("height", d => height - yMap(d));
+        //Add bars
+      	svg.selectAll(".bar")
+      	  .data(cont)
+      	  .enter().append("rect")
+      	  .style("fill", barColor)
+      	  .attr("class", "bar")
+      	  .attr("x", d => xMap(d))
+          .attr("y", d => yMap(d))
+      	  .attr("width", xScale.bandwidth())
+      	  .attr("height", d => height - yMap(d))
+          .on("mousemove", function(d) {
+            tooltip.html(Number.parseFloat(yValue(d)).toFixed(2) + "%")
+              .style("left", (d3.mouse(this)[0] + 10) + "px")
+              .style("top", d3.mouse(this)[1] + "px")
+              .style("opacity", 1);
+          })
+          .on("mouseout", function(d) {
+            tooltip.style("opacity", 0);
+          });
 
-      //Add cuttoff line
-    	svg.append("line")
-    	  .attr("x1", 0)
-    	  .attr("x2", width)
-    	  .attr("y1", yScale(thresh[0]))
-    	  .attr("y2", yScale(thresh[0]))
-    	  .style("stroke", "darkred")
-    	  .style("stroke-width", 1.5)
-    	  .style("stroke-dasharray", ("3, 3"));
+        //Add cuttoff line
+      	svg.append("line")
+      	  .attr("x1", 0)
+      	  .attr("x2", width)
+      	  .attr("y1", yScale(thresh[0]))
+      	  .attr("y2", yScale(thresh[0]))
+      	  .style("stroke", "darkred")
+      	  .style("stroke-width", 1.5)
+      	  .style("stroke-dasharray", ("3, 3"));
 
-      //Plot title
-    	svg.append("text")
-        .attr("x", (100)) // I'm not sure on this
-        .attr("y", 0 - (margin.top / 3))
-        .attr("text-anchor", "middle")
-        .style("font-size", "16px")
-        .text("Contribution (%) of Variables to " + component);
+        //Plot title
+      	svg.append("text")
+          .attr("x", (100)) // I'm not sure on this
+          .attr("y", 0 - (margin.top / 3))
+          .attr("text-anchor", "middle")
+          .style("font-size", "16px")
+          .text("Contribution (%) of Variables to " + component);
 
-      //Return button
-      svg.append("text")
-        .attr("x", (width - width / 10)) // Or this
-        .attr("y", 0 - (margin.top / 3))
-        .attr("text-anchor", "middle")
-        .style("font-size", "12px")
-        .style("text-decoration", "underline")
-        .style("cursor", "pointer")
-        .on("click", function() {
-          d3.select("svg").selectAll("*").transition()
-            .duration(500)
-            .style("opacity", 0)
-            .remove();
-            instance.pcaPlot(instance);
-        	})
-        .text("Return");
+        //Return button
+        svg.append("text")
+          .attr("x", (width - width / 10)) // Or this
+          .attr("y", 0 - (margin.top / 3))
+          .attr("text-anchor", "middle")
+          .style("font-size", "12px")
+          .style("text-decoration", "underline")
+          .style("cursor", "pointer")
+          .on("click", function() {
+            d3.select("svg").selectAll("*")
+              .remove();
+              instance.pcaPlot(instance);
+          	})
+          .text("Return");
       }
     };
   },
